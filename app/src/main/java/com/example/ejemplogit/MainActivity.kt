b83483150aa9bcb03d389bb38f842fe5fcc99899
Federico Navarro <navarrofederico14@gmail.com>
2022-09-29 23:28:50 -0300

    probando cambios

--- app/src/main/java/com/example/ejemplogit/MainActivity.kt
+++ app/src/main/java/com/example/ejemplogit/MainActivity.kt
@@ -7,5 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        print("estoy probando algunas funciones de android con git")
     }
 }
