f3b9436bebce86867ef839f6511b36c578c3a4a6
Federico Navarro <navarrofederico14@gmail.com>
2022-09-29 23:48:54 -0300

    subo feature en rama feature

--- app/src/main/java/com/example/ejemplogit/MainActivity.kt
+++ app/src/main/java/com/example/ejemplogit/MainActivity.kt
@@ -7,8 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        print("estoy probando algunas funciones de android con git")
-        println("ya estoy en la rama de develop")
+      print("cree una feature")
 
 
 
